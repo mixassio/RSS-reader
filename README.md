d2c5d348569e4ce39654ea456dff84f646b8e06e
mixassio <mixail.bondarev@gmail.com>
2018-11-26 14:11:32 +0300

    the app is run

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# project-lvl3-s354" 
+# project-lvl3-s354
+
+[![Build Status](https://travis-ci.org/mixassio/project-lvl3-s354.svg?branch=master)](https://travis-ci.org/mixassio/project-lvl3-s354)
+
---
+++ src/Example.js
@@ -0,0 +1,12 @@
+// @flow
+
+export default class Example {
+  constructor(element) {
+    this.element = element;
+  }
+
+  init() {
+    this.element.textContent = 'hello, world!';
+    console.log('ehu!');
+  }
+}

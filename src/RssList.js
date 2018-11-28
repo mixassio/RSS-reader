810ba299acbee67285726ca7667cd5bd1fa0ce61
mixassio <mixail.bondarev@gmail.com>
2018-11-28 11:41:37 +0300

    add render

---
+++ src/RssList.js
@@ -0,0 +1,60 @@
+
+export default class RssList {
+  constructor(element) {
+    this.element = element;
+  }
+
+  init() {
+    this.element.textContent = 'There will be your rss-chanell';
+  }
+
+  addChanel({
+    id, title, link, news,
+  }) {
+    const cardEl = document.createElement('div');
+    cardEl.classList.add('card');
+    const cardHeadEl = document.createElement('div');
+    cardHeadEl.setAttribute('id', `heading${id}`);
+    cardHeadEl.setAttribute('class', 'card-header');
+    cardEl.appendChild(cardHeadEl);
+    const h5El = document.createElement('h5');
+    h5El.classList.add('mb-0');
+    cardHeadEl.appendChild(h5El);
+    const buttonEl = document.createElement('button');
+    buttonEl.classList.add('btn', 'btn-link');
+    buttonEl.setAttribute('type', 'button');
+    buttonEl.setAttribute('data-toggle', 'collapse');
+    buttonEl.setAttribute('data-target', `#collapse${id}`);
+    buttonEl.setAttribute('aria-expanded', 'true');
+    buttonEl.setAttribute('aria-controls', `collapse${id}`);
+    buttonEl.textContent = title;
+    h5El.appendChild(buttonEl);
+    const collapseEl = document.createElement('div');
+    collapseEl.setAttribute('id', `collapse${id}`);
+    collapseEl.setAttribute('class', 'collapse');
+    collapseEl.setAttribute('aria-labelledby', `heading${id}`);
+    collapseEl.setAttribute('data-parent', '#accordionExample');
+    cardEl.appendChild(collapseEl);
+    const cardBodyEl = document.createElement('div');
+    cardBodyEl.classList.add('card-body');
+    collapseEl.appendChild(cardBodyEl);
+    const ulNewsEl = document.createElement('ul');
+    ulNewsEl.classList.add('list-group');
+    cardBodyEl.appendChild(ulNewsEl);
+    news.forEach((element) => {
+      const liNewsEl = document.createElement('li');
+      liNewsEl.classList.add('list-group-item');
+      liNewsEl.textContent = element.title;
+      ulNewsEl.appendChild(liNewsEl);
+    });
+    this.element.appendChild(cardEl);
+  }
+
+  render(state) {
+    const { chanels } = state;
+    chanels.forEach((chanel) => {
+      this.addChanel(chanel);
+    });
+    return this.element;
+  }
+}
--- src/init.js
+++ src/init.js
@@ -1,7 +1,32 @@
-import Example from './Example';
+import RssList from './RssList';
+import exampleState from './stateExample';
+
+const state = {
+  chanels: [
+    {
+      id: 1,
+      title: 'title chanel',
+      link: 'www.hexlet.io',
+      news: [
+        {
+          id: 1,
+          title: 'title news',
+          description: 'description news',
+          link: 'www.hexlet.io',
+        },
+      ],
+    },
+  ],
+  registrationProcess: {
+    valid: true,
+    submitDisabled: true,
+  },
+};
+
 
 export default () => {
-  const element = document.getElementById('point');
-  const obj = new Example(element);
+  const element = document.getElementById('accordionExample');
+  const obj = new RssList(element);
   obj.init();
+  obj.render(exampleState);
 };
